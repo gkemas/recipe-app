a981e22543ff01b37c42d7b72cbaacc807a9be28
gokhan <kemasgokhan@gmail.com>
2022-07-16 10:46:48 +0300

    ad

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,11 +11,11 @@ const Home = () => {
   const [selectedMeal, setSelectedMeal] = useState(mealType[0]);
   const [recipes, setRecipes] = useState(null);
 
-  // const APP_ID = process.env.REACT_APP_APP_ID;
-  // const APP_KEY = process.env.REACT_APP_APP_KEY;
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const APP_KEY = process.env.REACT_APP_APP_KEY;
 
-  // const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
-  const url = `https://api.edamam.com/search?q=${query}&app_id=dd81734d&app_key=b8a952198128b93d1604257ef80f6172&mealType=${selectedMeal}`;
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+  // const url = `https://api.edamam.com/search?q=${query}&app_id=dd81734d&app_key=b8a952198128b93d1604257ef80f6172&mealType=${selectedMeal}`;
 
   const getData = async () => {
     if (query) {
